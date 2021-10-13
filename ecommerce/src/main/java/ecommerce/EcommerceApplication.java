5cfa1bb9a1bf4a23b8840eb26623a88b6bfe5957
mdjoo0810 <mdjoo0810@gmail.com>
2021-10-13 18:04:57 +0900

    feat: root module setting

---
+++ ecommerce/src/main/java/ecommerce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcommerceApplication.class, args);
+  }
+
+}
